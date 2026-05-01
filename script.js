d55663fa3bc9b388341c3cc3a29f787712a7f9ec
sh333z <sheezmk13@gmail.com>
2026-05-01 12:24:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const navLinks = document.querySelector('.nav-links');
 menuBtn.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+const track = document.querySelector(".carousel-track");
+const cards = document.querySelectorAll(".card");
+
+let index = 2; // center starts here
+
+function updateCarousel() {
+  cards.forEach(card => card.classList.remove("active"));
+
+  cards[index].classList.add("active");
+
+  const offset = cards[index].offsetLeft - (window.innerWidth / 2 - cards[index].offsetWidth / 2);
+  track.style.transform = `translateX(-${offset}px)`;
+}
+
+/* Auto slide */
+setInterval(() => {
+  index++;
+
+  if (index >= cards.length) {
+    index = 0;
+  }
+
+  updateCarousel();
+}, 2500);
+
+updateCarousel();
