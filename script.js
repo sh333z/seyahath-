c5c5a5505889753420c1d6d40703355e16681618
sh333z <sheezmk13@gmail.com>
2026-05-01 23:28:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,53 @@ menuBtn.addEventListener('click', () => {
 // =======================
 // CAROUSEL
 // =======================
+const track = document.querySelector(".carousel-track");
+let cards = document.querySelectorAll(".card");
 
+const cardWidth = 410; // 380 + 30 gap
+const visibleCards = 3;
+let index = visibleCards;
+
+/* CLONE */
+for (let i = 0; i < visibleCards; i++) {
+  const firstClone = cards[i].cloneNode(true);
+  const lastClone = cards[cards.length - 1 - i].cloneNode(true);
+
+  track.appendChild(firstClone);
+  track.prepend(lastClone);
+}
+
+cards = document.querySelectorAll(".card");
+
+/* INITIAL POSITION */
+track.style.transform = `translateX(-${cardWidth * index}px)`;
+
+/* AUTO SLIDE */
+setInterval(() => {
+  index++;
+
+  track.style.transition = "transform 0.6s cubic-bezier(0.25, 0.8, 0.25, 1)";
+  track.style.transform = `translateX(-${cardWidth * index}px)`;
+
+  /* RESET LOOP */
+  
+  if (index >= cards.length - visibleCards) {
+  setTimeout(() => {
+
+    track.style.transition = "none";
+
+    index = index - cards.length + (visibleCards * 2);
+
+    track.style.transform = `translateX(-${cardWidth * index}px)`;
+
+    // 🔥 FORCE browser to apply instantly (important for Chrome)
+    track.offsetHeight;
+
+    requestAnimationFrame(() => {
+      track.style.transition = "transform 0.6s cubic-bezier(0.25, 0.8, 0.25, 1)";
+    });
+
+  }, 600);
+}
+
+}, 3500);
